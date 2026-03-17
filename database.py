bc3efbbb601f66c9ae215c8409f3179d9ddc957b
Fernando Vergara <vergaraxito@gmail.com>
2026-03-17 15:22:41 -0300

    usar secrets en streamlit cloud

--- database.py
+++ database.py
@@ -1,11 +1,17 @@
-import psycopg2
 import os
+import streamlit as st
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def conectar():
+    database_url = st.secrets.get("DATABASE_URL", os.getenv("DATABASE_URL"))
+
+    if not database_url:
+        raise ValueError("DATABASE_URL no está configurada")
+
     return psycopg2.connect(
-        os.getenv("DATABASE_URL"),
+        database_url,
         sslmode="require"
     )
